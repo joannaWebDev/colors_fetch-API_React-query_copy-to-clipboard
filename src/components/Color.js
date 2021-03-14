46d017b6194ad411acc6d549f51cef9b745f5399
unknown <joannasmerea@gmail.com>
2021-03-14 15:20:14 +0100

    added background prop to each fetched item

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {IoMdCopy} from 'react-icons/io';
+import { IoMdCopy } from 'react-icons/io';
 
 const Color = ({ color }) => {
   const aioColors = document.querySelectorAll('.color p');
+  console.log(aioColors);//GETTING A NODELIST I need the textContent
 
   aioColors.forEach((color) => {
     color.addEventListener('click', () => {
@@ -13,11 +14,12 @@ const Color = ({ color }) => {
       selection.addRange(range);
 
       try {
-        document.execCommand('copy');
+        document.execCommand('copy');        
         selection.removeAllRanges();
 
         const original = color.textContent;
         color.textContent = 'Copied!';
+        console.log(color);
         color.classList.add('success');
 
         setTimeout(() => {
@@ -36,20 +38,21 @@ const Color = ({ color }) => {
   });
 
   return (
-    <div className="color m-3 box-border w-3/4 md:w-1/3 p-4 border-4 flex flex-col shadow-2xl hover:bg-blue-100 cursor-pointer">
-      <div><IoMdCopy className="text-pink-500 float-right"/></div>
+    <div className="color m-3 box-border w-3/4 md:w-1/3 p-4 border-4 flex flex-col shadow-md hover:shadow-2xl cursor-pointer" style={{backgroundColor:color.color}}>
+      <div>
+        <IoMdCopy className="float-right" />
+      </div>
       <p className="items-start">{color.year}</p>
-      <div className="text-center">
+      <div className="text-center" >
         <p>{color.name}</p>
         <p className="font-bold">{color.color}</p>
       </div>
-      <p className="text-right items-end">{color.pantone_value}</p>
-      <p class="error-msg hide">Can't copy, hit Ctrl+C!</p>
+        <p className="text-right items-end">{color.pantone_value}</p>
+      <p className="error-msg hide">Can't copy, hit Ctrl+C!</p>
     </div>
   );
 };
 export default Color;
-
 
 /*IMPORTANT 
 --> const selection = window.getSelection();
